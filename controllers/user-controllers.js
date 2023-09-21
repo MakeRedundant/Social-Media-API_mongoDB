57cc414a2044183a49893aed133759f05f373a80
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-21 23:29:36 +1000

    started thoughts

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,1 +1,14 @@
-console.log("dadkaj");
+const { Thought } = require('../models/Thought');
+const User = require('../models/User');
+
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            // .populate('reactionId')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
