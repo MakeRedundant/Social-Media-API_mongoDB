88266440845ab075907845ffd61955f2b872808b
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-22 23:04:16 +1000

    added userControllers.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,8 @@
 const User = require('./user');
 const Thought = require('./thought');
+// const Reaction = require('./thought');
 
 module.exports = { User, Thought };
+
+//No need to export the reaction model since it is primarily associated with
+//the thoughts model as a nested schema.
