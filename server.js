c1bf9f1db18a6eb1121346684912009902ccf0a0
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-23 01:53:13 +1000

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -7,13 +8,17 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('01-Activities')[1]
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('Social-Media-API')
+  ? cwd.split('Social-Media-API')[1]
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 db.once('open', () => {
   app.listen(PORT, () => {
